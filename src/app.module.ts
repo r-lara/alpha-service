110b6a1864ca27219bd70a1f6efa66ec6afc38bb
Ricardo Lara <rlara@zellship.com>
2023-09-13 18:50:20 -0600

    feat: initiate services resource

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
 import { UsersModule } from './users/users.module';
+import { ServicesModule } from './services/services.module';
 
 @Module({
-  imports: [AuthModule, UsersModule],
+  imports: [AuthModule, UsersModule, ServicesModule],
   controllers: [AppController],
   providers: [AppService],
 })
